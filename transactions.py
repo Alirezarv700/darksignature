6106e34786bece320d64f8bfbad9c760c82d9824
Alirezarv700 <84043701+Alirezarv700@users.noreply.github.com>
2025-04-06 19:06:42 +0330

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -9,7 +9,7 @@ for i in range(32):
     process = subprocess.run(
         ["./darksignature", "-pubkey",
          "ca5606a1e820e7a2f6bb3ab090e8ade7b04a7e0b5909a68dda2744ae3b8ecbfa",
-         "b90fdec161ad6ef4378f274a60b900452431533596bf3bd23e01202ebf679461"],
+         "280a47639c811134d648e8ee8096c33b41611be509ebca837fbda10baaa1eb15"],
         stdout=subprocess.PIPE,
         universal_newlines=True
     )
